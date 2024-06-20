42863ffc73312c4096a6905f7b58330afc5ab1e9
Warren Hong <hwhong1129@gmail.com>
2024-06-19 22:00:11 -0700

    v2 animation trove. making prorgess

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,16 +1,65 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./main.module.css";
 import { GeistMono } from "geist/font/mono";
 import classNames from "classnames";
+import { motion, AnimatePresence } from "framer-motion";
+import { useOnClickOutside } from "usehooks-ts";
 
 export function Main() {
+  const [selectedItem, setSelectedItem] = useState<null | number>(null);
+  const ref = useRef(null);
+  // useOnClickOutside(ref, () => setSelectedItem(null));
+
+  useEffect(() => {
+    function onKeyDown(event: any) {
+      if (event.key === "Escape") {
+        setSelectedItem(null);
+      }
+    }
+
+    window.addEventListener("keydown", onKeyDown);
+    return () => window.removeEventListener("keydown", onKeyDown);
+  }, []);
+
+  const onItemClick = (index: number) => setSelectedItem(index);
+
   return (
-    <div className={styles.root}>
-      {Array.from(Array(100).keys()).map((i) => (
-        <div key={i} className={classNames(styles.block, GeistMono.className)}>
-          {i}
-        </div>
-      ))}
-    </div>
+    <>
+      <AnimatePresence>
+        {selectedItem ? (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className={styles.overlay}
+          />
+        ) : null}
+      </AnimatePresence>
+      <AnimatePresence>
+        {selectedItem && (
+          <motion.div
+            layoutId={`selectedIdx-${selectedItem}`}
+            className={styles.modal}
+            ref={ref}
+            initial={{ scale: 0.7 }}
+            animate={{ scale: 1 }}
+          >
+            {selectedItem}
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <div className={styles.root}>
+        {Array.from(Array(100).keys()).map((i) => (
+          <motion.div
+            key={i}
+            layoutId={`selectedIdx-${selectedItem}`}
+            className={classNames(styles.block, GeistMono.className)}
+            onClick={() => onItemClick(i)}
+          >
+            {i}
+          </motion.div>
+        ))}
+      </div>
+    </>
   );
 }
