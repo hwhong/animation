1a684671491de0586bf84d0b29be8d6790e197d5
Warren Hong <hwhong1129@gmail.com>
2024-07-01 22:14:20 -0700

    feat: drag carousel complete

--- src/v2/11/drag-carousel.tsx
+++ src/v2/11/drag-carousel.tsx
@@ -1,18 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./drag-carousel.module.css";
+import { motion, useMotionValue } from "framer-motion";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import classNames from "classnames";
 
 export function DragCarousel() {
+  const [idx, setIdx] = useState(0);
+  /**
+   * All motion components internally use MotionValues to track the state and velocity of an animating value.
+   * useMotionValue injects that value in manually via the style prop.
+   * e.g can manually set and get it
+   *    dragX.get() //100
+   *    dragX.set(100)
+   *    dragX.onChange(...)
+   */
+  const dragX = useMotionValue(0);
+  const DRAG_BUFFER = 50;
   const COLORS = ["#32ADE6", "#FFCC00", "#34C759"];
+
+  const onDragEnd = () => {
+    const x = dragX.get();
+
+    if (x <= -DRAG_BUFFER && idx < COLORS.length - 1) {
+      setIdx((pv) => pv + 1);
+    } else if (x >= DRAG_BUFFER && idx > 0) {
+      setIdx((pv) => pv - 1);
+    }
+  };
+
+  const onLeftClick = () => {
+    if (idx > 0) {
+      setIdx((pv) => pv - 1);
+    }
+  };
+
+  const onRightClick = () => {
+    if (idx < COLORS.length - 1) {
+      setIdx((pv) => pv + 1);
+    }
+  };
 
   return (
     <div className={styles.root}>
+      <div
+        className={classNames(styles.arrow, styles.left)}
+        onClick={onLeftClick}
+      >
+        <ChevronLeftIcon />
+      </div>
       <div className={styles.carousel}>
         {COLORS.map((color) => (
-          <div
+          <motion.div
+            drag="x"
             key={color}
-            style={{ backgroundColor: color }}
+            dragConstraints={{
+              left: 0,
+              right: 0,
+            }}
+            style={{
+              x: dragX,
+              backgroundColor: color,
+            }}
+            animate={{
+              translateX: `-${idx * 100}%`,
+            }}
+            onDragEnd={onDragEnd}
             className={styles.slide}
-          ></div>
+            transition={{ bounce: 0 }}
+          />
+        ))}
+      </div>
+      <div
+        className={classNames(styles.arrow, styles.right)}
+        onClick={onRightClick}
+      >
+        <ChevronRightIcon />
+      </div>
+      <div className={styles.dots}>
+        {Array.from(Array(COLORS.length).keys()).map((dotIdx) => (
+          <div
+            key={dotIdx}
+            className={classNames(styles.dot, {
+              [styles.active]: idx === dotIdx,
+            })}
+          />
         ))}
       </div>
     </div>
