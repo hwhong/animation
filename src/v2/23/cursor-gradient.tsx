0d323516d76373bcb62ad543538dfd37808214fe
Warren Hong <hwhong1129@gmail.com>
2024-07-16 23:01:54 -0700

    fix: useSpring makes everything smoother

--- src/v2/23/cursor-gradient.tsx
+++ src/v2/23/cursor-gradient.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import styles from "./cursor-gradient.module.css";
-import { motion, useMotionTemplate, useMotionValue } from "framer-motion";
+import {
+  motion,
+  useMotionTemplate,
+  useMotionValue,
+  useSpring,
+} from "framer-motion";
 
 export function CursorGradient() {
   const x = useMotionValue(0);
   const y = useMotionValue(0);
+
+  const nX = useSpring(x);
+  const nY = useSpring(y);
+
   const onMouseMove = ({ clientX, clientY, currentTarget }: any) => {
     const { left, top } = (currentTarget as any).getBoundingClientRect();
 
@@ -17,7 +26,7 @@ export function CursorGradient() {
       className={styles.root}
       onMouseMove={onMouseMove}
       style={{
-        background: useMotionTemplate`radial-gradient(circle at ${x}px ${y}px, #f4f4f4,  #b4b4b4)`,
+        background: useMotionTemplate`radial-gradient(circle at ${nX}px ${nY}px, #f4f4f4,  #b4b4b4)`,
       }}
     ></motion.div>
   );
