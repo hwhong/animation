719d8b6d46e57a7c53e59308070b379cc84dce27
Warren Hong <hwhong1129@gmail.com>
2024-08-07 22:33:07 -0700

    feat: scroll frame progress

--- src/v2/40/scroll-frame.tsx
+++ src/v2/40/scroll-frame.tsx
@@ -1,24 +1,47 @@
 import React, { useRef } from "react";
 import styles from "./scroll-frame.module.css";
-import { motion, useMotionValue, useScroll, useTransform } from "framer-motion";
+import {
+  motion,
+  useMotionValue,
+  MotionValue,
+  useScroll,
+  useTransform,
+} from "framer-motion";
+import { random } from "@/utils/utility";
+
+const END_THRESHOLD = 200;
 
 export function ScrollFrame() {
   const ref = useRef(null);
   const { scrollY } = useScroll({
     container: ref,
   });
-  const xVal = useTransform(scrollY, [0, 200], [50, 0]);
-  const yVal = useTransform(scrollY, [0, 200], [100, 0]);
-  const rotate = useTransform(scrollY, [0, 200], [35, 0]);
 
   return (
     <motion.div className={styles.root} ref={ref}>
       <div className={styles.wrapper}>
-        <motion.div
-          className={styles.frame1}
-          style={{ marginTop: xVal, marginLeft: yVal, rotate }}
-        ></motion.div>
+        {Array.from(Array(10).keys()).map((i) => {
+          return <Block key={i} scrollY={scrollY} />;
+        })}
       </div>
     </motion.div>
+  );
+}
+
+interface BlockProps {
+  scrollY: MotionValue<number>;
+}
+
+function Block({ scrollY }: BlockProps) {
+  const ranX = random(0, 300);
+  const ranY = random(100, 150);
+  const xVal = useTransform(scrollY, [0, END_THRESHOLD], [ranX, 0]);
+  const yVal = useTransform(scrollY, [0, END_THRESHOLD], [ranY, 0]);
+  const rotate = useTransform(scrollY, [0, END_THRESHOLD], [35, 0]);
+  return (
+    <motion.div
+      className={styles.frame1}
+      style={{ translateX: xVal, translateY: yVal, rotate }}
+    ></motion.div>
   );
 }
