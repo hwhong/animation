e1ce14176c9e7ea562567262f352925c58842d56
Warren Hong <hwhong1129@gmail.com>
2024-08-20 21:17:20 -0700

    feat: progress

--- src/v2/45/stack-interaction.tsx
+++ src/v2/45/stack-interaction.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import styles from "./stack-interaction.module.css";
+import { motion } from "framer-motion";
+
+const CARD_OFFSET = 10;
+const SCALE_FACTOR = 0.06;
 
 export function StackInteraction() {
+  const colors = ["#00b2ff", "#01c947", "#feb90c"];
+
   return (
     <div className={styles.root}>
-      {Array.from(Array(3).keys()).map((i) => (
-        <div key={i} className={styles.panel}></div>
+      {colors.map((color, index) => (
+        <motion.div
+          key={color}
+          className={styles.panel}
+          style={{ backgroundColor: color }}
+          animate={{
+            top: index * -CARD_OFFSET,
+            scale: 1 - index * SCALE_FACTOR,
+            zIndex: colors.length - index,
+          }}
+        ></motion.div>
       ))}
     </div>
   );
